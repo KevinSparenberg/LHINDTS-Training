fc0a0cfffb2b2ee5b0f0856ab1144e9486b9b8d9
Renas Derwish <renas.derwish@hotmail.de>
2018-11-06 13:06:50 +0100

    delete shoppingcart from app.module.ts and test the login.html

--- food-delivery/view/food-delivery/src/app/app.module.ts
+++ food-delivery/view/food-delivery/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule
